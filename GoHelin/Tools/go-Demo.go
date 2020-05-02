1b76a7f51692202d40134f374cc301f1d31fa61a
FinalFantasyIII <1053206020@qq.com>
2020-05-02 13:23:13 +0800

    dag

--- GoHelin/Tools/go-Demo.go
+++ GoHelin/Tools/go-Demo.go
@@ -312,3 +312,119 @@ func goHttpPost(){
 
 }
 //################################################################################
+
+
+
+func SendEmail(){
+	//-----------------sync包与锁:限制线程对变量的访问------------------
+	//-----------------big包:对整数的高精度计算------------------------
+	//-----------------image包:图像包制作GIF动画----------------------
+	//-----------------regexp包:正则表达式---------------------------
+	//-----------------os包：系统相关，包括信号文件创建等---------------
+	//-----------------time包:时间相关,时间戳，定时器，计时器等----------
+	//-----------------flag包：命令行参数解析--------------------------
+	//-----------------go 发送电子邮件-------------------------------
+	fromEmail := "1053206020@qq.com"
+	toEmail := "1461671786@qq.com"
+	m := gomail.NewMessage()
+	m.SetAddressHeader("From", fromEmail, "贺林")
+	m.SetAddressHeader("To", toEmail, "helin")
+	m.SetHeader("Subject", "邮件测试")
+
+	s,err := ioutil.ReadFile("Report.html")
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	message := string(s)
+	fmt.Println(len(message))
+	m.SetBody("text/html", message)
+
+	d := gomail.NewDialer("smtp.qq.com", 587, "1053206020@qq.com", "srusxeiybntsbcac")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Printf("***%s\n", err.Error())
+	}
+}
+
+func makePGN(){
+	err := qrcode.WriteFile("https://www.ali213.net/",qrcode.Medium,256,"yx.png")
+	if err != nil{
+		fmt.Println(err)
+	}
+	s := "D:/this/is/video/mm.mp4"
+	n := strings.ReplaceAll(s,"/","\\")
+	fmt.Println(n)
+}
+
+func testUUID() {
+	// 创建
+	u1 ,_:= uuid.NewV4()
+	fmt.Printf("UUIDv4: %s\n", u1)
+
+	// 解析
+	u2, err := uuid.FromString("f5394eef-e576-4709-9e4b-a7c231bd34a4")
+	if err != nil {
+		fmt.Printf("Something gone wrong: %s", err)
+		return
+	}
+	fmt.Printf("Successfully parsed: %s", u2)
+}
+
+// twitter 雪花算法
+// 把时间戳,工作机器ID, 序列号组合成一个 64位 int
+// 第一位置零, [2,42]这41位存放时间戳,[43,52]这10位存放机器id,[53,64]最后12位存放序列号
+var (
+	machineID     int64 // 机器 id 占10位, 十进制范围是 [ 0, 1023 ]
+	sn            int64 // 序列号占 12 位,十进制范围是 [ 0, 4095 ]
+	lastTimeStamp int64 // 上次的时间戳(毫秒级), 1秒=1000毫秒, 1毫秒=1000微秒,1微秒=1000纳秒
+)
+
+func init() {
+	lastTimeStamp = time.Now().UnixNano() / 1000000
+}
+
+//设置机器码，防止生成重复编码
+func SetMachineId(mid int64) {
+	// 把机器 id 左移 12 位,让出 12 位空间给序列号使用
+	machineID = mid << 12
+}
+
+func GetSnowflakeId() int64 {
+	curTimeStamp := time.Now().UnixNano() / 1000000
+	// 同一毫秒
+	if curTimeStamp == lastTimeStamp {
+		sn++
+		// 序列号占 12 位,十进制范围是 [ 0, 4095 ]
+		if sn > 4095 {
+			time.Sleep(time.Millisecond)
+			curTimeStamp = time.Now().UnixNano() / 1000000
+			lastTimeStamp = curTimeStamp
+			sn = 0
+		}
+		// 取 64 位的二进制数 0000000000 0000000000 0000000000 0001111111111 1111111111 1111111111  1 ( 这里共 41 个 1 )和时间戳进行并操作
+		// 并结果( 右数 )第 42 位必然是 0,  低 41 位也就是时间戳的低 41 位
+		rightBinValue := curTimeStamp & 0x1FFFFFFFFFF
+		// 机器 id 占用10位空间,序列号占用12位空间,所以左移 22 位; 经过上面的并操作,左移后的第 1 位,必然是 0
+		rightBinValue <<= 22
+		id := rightBinValue | machineID | sn
+		return id
+	}
+
+	if curTimeStamp > lastTimeStamp {
+		sn = 0
+		lastTimeStamp = curTimeStamp
+		// 取 64 位的二进制数 0000000000 0000000000 0000000000 0001111111111 1111111111 1111111111  1 ( 这里共 41 个 1 )和时间戳进行并操作
+		// 并结果( 右数 )第 42 位必然是 0,  低 41 位也就是时间戳的低 41 位
+		rightBinValue := curTimeStamp & 0x1FFFFFFFFFF
+		// 机器 id 占用10位空间,序列号占用12位空间,所以左移 22 位; 经过上面的并操作,左移后的第 1 位,必然是 0
+		rightBinValue <<= 22
+		id := rightBinValue | machineID | sn
+		return id
+	}
+
+	if curTimeStamp < lastTimeStamp {
+		return 0
+	}
+	return 0
+}
